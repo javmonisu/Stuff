b1670d0c323f6a786670110ebb0d53cf54716dd7
Javier Montero <javier1689@gmail.com>
2013-09-27 11:19:13 +0200

    Learning C# - Encapsulation

---
+++ Cs101/Encapsulation/Encapsulation/Human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Human
+    {
+        private int eyes;
+
+        public int GetEyes()
+        {
+            return eyes;
+        }
+        public void SetEyes(int eyes)
+        {
+            this.eyes = eyes;
+        }
+    }
+}
---
+++ Cs101/Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human h = new Human();
+            h.SetEyes(2);
+            Console.WriteLine(h.GetEyes());
+            Console.ReadKey();
+        
+        }
+    }
+}
