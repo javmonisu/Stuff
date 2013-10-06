17e361f765eed7d1054892e880b903934e899d90
Javier Montero <javier1689@gmail.com>
2013-10-06 21:14:07 +0200

    Learning C# - Interfaces / Inherit

---
+++ Cs101/Interfaces/Interfaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+       
+    class Program : INumber
+    {
+        static int number;
+        static void Main(string[] args)
+        {
+            number = 1; 
+        }
+         public void setNumber(int num){
+            number = num;
+        }
+         public int getNumber(){
+            return number;
+        }
+    }
+}
