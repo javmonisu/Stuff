1e3f7a95dd9aec7e5020ede5ffd85b0db53347cf
Javier Montero <javier1689@gmail.com>
2013-10-06 20:39:26 +0200

    Learning C# - Function polimorfism

---
+++ Cs101/Polimorfismo/Polimorfismo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class Print
+    {
+        public void number(int number)
+        {
+            Console.WriteLine("Printing integer {0}", number);
+        }
+        public void number(double number)
+        {
+            Console.WriteLine("Printing float {0}", number);
+        }
+        public void number(string number)
+        {
+            Console.WriteLine("Printing string {0}", number);
+        }
+    }
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            staticPolimorphism();
+            
+        }
+       
+        public static void staticPolimorphism(){
+            //Function overloading.
+            Print p = new Print();
+            p.number(1);
+            p.number(1.5);
+            p.number("hola");
+            Console.ReadKey();
+        }
+    }
+}
