83299bde4cd43f473acf31c4326439e3f854ea6a
Javier Montero <javier1689@gmail.com>
2013-10-14 22:48:50 +0200

    Organizado el directorio

---
+++ University/Supuesto1Caso2/CapaDatos/API/GestionPlantilla.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos.Entities;
+namespace CapaDatos.API
+{
+    class GestionPlantilla
+    {
+        private List<Jugador> jugadores;
+
+        public List<Jugador> Jugadores
+        {
+            get { return jugadores; }
+            set { jugadores = value; }
+        }
+        private List<PlantillaTecnica> equipoTecnico;
+
+        public List<PlantillaTecnica> EquipoTecnico
+        {
+            get { return equipoTecnico; }
+            set { equipoTecnico = value; }
+        }
+    }
+}
---
+++ University/Supuesto1Caso2/InterfazUsuarioWinForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("InterfazUsuarioWinForms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InterfazUsuarioWinForms")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible como true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+[assembly: Guid("2e5ce0cd-1e40-42a1-abd2-52768163f1e9")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
